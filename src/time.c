24ec054a98d2f78ae387a2ead0a4721e1ba2455c
JLL32 <eabduljalil@outlook.com>
2021-10-18 12:46:36 +0100

    normes time.c/h

--- src/time.c
+++ src/time.c
@@ -1,28 +1,30 @@
 #include "time.h"
 #include <stddef.h>
 
-void block_thread(size_t ms)
+void	block_thread(size_t ms)
 {
-	const size_t micro_s = ms * 1000;
+	const size_t	micro_s = ms * 1000;
+
 	usleep(micro_s - 2e3);
 }
 
-size_t get_time()
+size_t	get_time(void)
 {
-	struct timeval time;
+	struct timeval	time;
 
 	gettimeofday(&time, NULL);
 	return (time.tv_sec * 1000 + time.tv_usec / 1000);
 }
 
-size_t time_elapsed(size_t starting_time)
+size_t	time_elapsed(size_t starting_time)
 {
 	return (get_time() - starting_time);
 }
 
-size_t remaining_time(const t_philo *philo)
+size_t	remaining_time(const t_philo *philo)
 {
-	const size_t elapsed = time_elapsed(philo->starting_time);
+	const size_t	elapsed = time_elapsed(philo->starting_time);
+
 	if (philo->life_time > elapsed)
 		return (philo->life_time - elapsed);
 	else
