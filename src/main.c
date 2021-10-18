f9f6d49581f827eb1665f89f064c603546783aa9
JLL32 <eabduljalil@outlook.com>
2021-10-18 12:34:27 +0100

    normes main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,9 @@
 #include "forks.h"
-#include "philo.h"
-#include "time.h"
 #include "parse_arg.h"
-#include "utils.h"
+#include "philo.h"
 #include "string_utils.h"
+#include "time.h"
+#include "utils.h"
 #include <malloc/_malloc.h>
 #include <pthread.h>
 #include <stdbool.h>
@@ -11,18 +11,18 @@
 #include <sys/_pthread/_pthread_mutex_t.h>
 #include <sys/_types/_size_t.h>
 
-t_data get_data(int argc, char** argv, int *err)
+t_data	get_data(int argc, char **argv, int *err)
 {
-	size_t n_philo;
-	size_t life_time;
-	size_t time_to_eat;
-	size_t time_to_sleep;
-	t_eating_times eating_times;
+	size_t			n_philo;
+	size_t			life_time;
+	size_t			time_to_eat;
+	size_t			time_to_sleep;
+	t_eating_times	eating_times;
 
 	if (argc < 5 || argc > 6)
 	{
 		*err = 1;
-		return (t_data){};
+		return ((t_data){});
 	}
 	n_philo = parse_arg_size_t(argv[1], err);
 	life_time = parse_arg_size_t(argv[2], err);
@@ -30,40 +30,45 @@ t_data get_data(int argc, char** argv, int *err)
 	time_to_sleep = parse_arg_size_t(argv[4], err);
 	if (argc == 5)
 	{
-		eating_times = (t_eating_times) {1, true};
+		eating_times = (t_eating_times){1, true};
 	}
 	else
 		eating_times = (t_eating_times){parse_arg_size_t(argv[5], err), false};
 	if (*err)
-		return (t_data){};
-	return ((t_data) {n_philo, life_time, time_to_eat, time_to_sleep, eating_times});
+		return ((t_data){});
+	return (
+		(t_data){n_philo, life_time, time_to_eat, time_to_sleep, eating_times});
 }
 
-t_philo *create_philos(t_data data)
+t_philo	*create_philos(t_data data)
 {
-	t_philo *philos;
-	size_t i;
+	t_philo	*philos;
+	size_t	i;
 
 	philos = malloc(data.n_philo * sizeof(*philos));
 	i = 0;
-	while(i < data.n_philo)
+	while (i < data.n_philo)
 	{
-		ft_memcpy(&philos[i], &(t_philo){ .id = i + 1, .thread_id = 0,
-			.time_to_eat = data.time_to_eat, .time_to_sleep = data.time_to_sleep,
+		ft_memcpy(&philos[i],
+			&(t_philo){
+			.id = i + 1, .thread_id = 0,
+			.time_to_eat = data.time_to_eat,
+			.time_to_sleep = data.time_to_sleep,
 			.life_time = data.life_time, .eating_times = data.eating_times,
-			.starting_time = 0, .first_starting_time = 0, .next = eating,
-			.l_fork = NULL, .r_fork = NULL, .env.display_mutex = NULL,
-			.env.philo_list = NULL,
-			.env.forks_list = NULL, .env.number_of_philos = 0,
-		}, sizeof(t_philo)); // NOTE: replace memcpy later
+			.starting_time = 0, .first_starting_time = 0,
+			.next = eating, .l_fork = NULL,
+			.r_fork = NULL, .env.display_mutex = NULL,
+			.env.philo_list = NULL, .env.forks_list = NULL,
+			.env.number_of_philos = 0, },
+			sizeof(t_philo));
 		i++;
 	}
-	return philos;
+	return (philos);
 }
 
-void await(t_philo *philo_list, size_t size)
+void	await(t_philo *philo_list, size_t size)
 {
-	size_t i;
+	size_t	i;
 
 	i = 0;
 	while (i < size)
@@ -73,19 +78,20 @@ void await(t_philo *philo_list, size_t size)
 	}
 }
 
-void start_simulation(t_data data,
-	t_philo *philo_list, pthread_mutex_t *forks_list, int *err)
+void	start_simulation(t_data data,
+	t_philo *philo_list,
+	pthread_mutex_t *forks_list,
+	int *err)
 {
-	pthread_mutex_t display;
-	bool stop;
-	size_t i;
-	const size_t starting_time = get_time();
+	pthread_mutex_t	display;
+	bool			stop;
+	size_t			i;
+	const size_t	starting_time = get_time();
 
 	stop = false;
-
 	pthread_mutex_init(&display, NULL);
 	i = 0;
-	while(i < data.n_philo)
+	while (i < data.n_philo)
 	{
 		philo_list[i].starting_time = starting_time;
 		philo_list[i].first_starting_time = starting_time;
@@ -96,24 +102,19 @@ void start_simulation(t_data data,
 		philo_list[i].env.display_mutex = &display;
 		philo_list[i].env.forks_list = forks_list;
 		philo_list[i].env.stop = &stop;
-		*err = pthread_create(&(philo_list[i].thread_id), NULL, init, &philo_list[i]);
-		if (*err)
-		{
-			free_all(&philo_list[i]);
-			return ;
-		}
+		pthread_create(&(philo_list[i].thread_id), NULL, init, &philo_list[i]);
 		i++;
 	}
 	await(philo_list, data.n_philo);
 	free_all(&philo_list[0]);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	static int err;
-	t_philo *philo_list;
-	pthread_mutex_t *forks_list;
-	const t_data data = get_data(argc, argv, &err);
+	static int		err;
+	t_philo			*philo_list;
+	pthread_mutex_t	*forks_list;
+	const t_data	data = get_data(argc, argv, &err);
 
 	if (err)
 		panic("Invalid arguments 😱");
@@ -126,7 +127,7 @@ int main(int argc, char **argv)
 		free(philo_list);
 		panic("Something went wrong with the forks 😱");
 	}
-	start_simulation(data,philo_list, forks_list, &err);
+	start_simulation(data, philo_list, forks_list, &err);
 	if (err)
 		panic("Something went wrong with the threads 😱");
 }
